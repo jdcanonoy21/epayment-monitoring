34c8961a26ec32975d376bb2b937ef95a6e271f2
Jessie Zamora <jzamss@gmail.com>
2021-02-16 16:49:44 +0800

    structure

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,49 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "./Content.css";
 
 const partners = [
-    {name: "Landbank"},
-    { name: "DBP" },
-    { name: "PayMaya" },
-    { name: "Gcash"}
+  { name: "Gcash" },
+  { name: "DBP" },
+  { name: "Landbank" },
+  { name: "PayMaya" },
 ];
 
 const ePayment = [
-    { lgu: 'Aklan', partner: ['1,000', '200', '400', '600','2,200'] },
-    { lgu: 'Legazpi', partner: ['100','12','500','200','812'] },
-    { lgu: 'Panglao', partner: ['88','12','200','200','500'] },
-    { lgu: 'Kalibo', partner: ['10','20','500','200','730'] }
-  ];
-
-
-
+  { lgu: "Aklan", partner: { landbank: 12, dbp: 50, paymaya: 30, gcash: 0 } },
+  { lgu: "Legazpi", partner: { landbank: 12, dbp: 50, paymaya: 30, gcash: 0 } },
+  // { lgu: "Legazpi", partner: ["100", "12", "500", "200", "812"] },
+  // { lgu: "Panglao", partner: ["88", "12", "200", "200", "500"] },
+  // { lgu: "Kalibo", partner: ["10", "20", "500", "200", "730"] },
+];
 
 const Monitoring = (props) => {
-    const [partner] = useState(partners); 
+  const [partner] = useState(partners);
 
   return (
     <div className="Content">
-		<div className="Content__header">
-			<p className="Content__lgu">LGU</p>
-			{partner.map( partner => (
-			  <p className="Content__partners">{partner.name}</p>
-			))}
-			<p className="Content__partners">TOTAL</p>
-		</div>
-        {
-          ePayment.map(item => (
-              <div className="Content__subContainer">
-              	<p className="Content__lgu">{item.lgu}</p>
-                {item.partner.map(partner => (
-	                <p className="Content__partners">{`${partner}`}</p>
-	            ))}
-              </div>
-          ))
-      	}
+      <div className="Content__header">
+        <p className="Content__lgu">LGU</p>
+        {partner.map((partner) => (
+          <p className="Content__partners">{partner.name}</p>
+        ))}
+        <p className="Content__partners">TOTAL</p>
+      </div>
+      {ePayment.map((payment) => {
+        return partner.map((partner) => {
+          return <div>{payment.partner[partner.name.toLowerCase()]} </div>;
+        });
+      })}
     </div>
   );
 };
-
-
 
 export default Monitoring;
