8b341fca60da8827b4074accec679e270f056a25
jdcanonoy21 <jdcanonoy21@gmail.com>
2021-02-17 16:31:01 +0800

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,40 +1,50 @@
 import React, { useState, useEffect } from "react";
 import "./Content.css";
 
-const partners = [
+const initialPartners = [
   { name: "Gcash" },
   { name: "DBP" },
   { name: "Landbank" },
   { name: "PayMaya" },
+  { name: "Total" },
 ];
 
-const ePayment = [
-  { lgu: "Aklan", partner: { landbank: 12, dbp: 50, paymaya: 30, gcash: 0 } },
-  { lgu: "Legazpi", partner: { landbank: 12, dbp: 50, paymaya: 30, gcash: 0 } },
-  // { lgu: "Legazpi", partner: ["100", "12", "500", "200", "812"] },
-  // { lgu: "Panglao", partner: ["88", "12", "200", "200", "500"] },
-  // { lgu: "Kalibo", partner: ["10", "20", "500", "200", "730"] },
+const initialEpayments = [
+  //{ lgu: "Aklan", partner: [ { landbank: 12 }, { dbp: 50 }, {paymaya: 30 }, { gcash: 0 } ] },
+  //{ lgu: "ALbay", partner: [ { landbank: 1 }, { dbp: 1 }, {paymaya: 1 }, { gcash: 1 } ] },
+  //{ lgu: "Legazpi", partner: [ { landbank: 2 }, { dbp: 2 }, {paymaya: 2 }, { gcash: 2 } ] },
+  { lgu: "Legazpi", partner: { landbank: 1, dbp: 1, paymaya: 1, gcash: 1, total:4 } },
+  { lgu: "Albay", partner: { landbank: 2, dbp: 2, paymaya:2, gcash: 0, total: 6 } },
+
 ];
 
-const Monitoring = (props) => {
-  const [partner] = useState(partners);
+const Content = (props) => {
+  const [partners, setPartners] = useState(initialPartners);
+  const [payments, setPayments] = useState(initialEpayments);
 
   return (
     <div className="Content">
+      <center><h1>ePayment Monitoring</h1></center>
       <div className="Content__header">
         <p className="Content__lgu">LGU</p>
-        {partner.map((partner) => (
+        {partners.map((partner) => (
           <p className="Content__partners">{partner.name}</p>
         ))}
-        <p className="Content__partners">TOTAL</p>
       </div>
-      {ePayment.map((payment) => {
-        return partner.map((partner) => {
-          return <div>{payment.partner[partner.name.toLowerCase()]} </div>;
+       {payments.map((payment) => {
+        const partnComponents =  partners.map((partner) => {
+          return <p className="Content__partners"> {payment.partner[partner.name.toLowerCase()]} </p>
         });
+        return (
+          <div className="Content__subContainer">
+            <p className="Content__lgu"> {payment.lgu} </p>
+            {partnComponents}
+          </div>
+        )
       })}
+      
     </div>
   );
 };
 
-export default Monitoring;
+export default Content;
